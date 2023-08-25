a91ed5ef4e93a3bdd76b5f34fb29124d54e00634
Omkar Gurav <omkargurav1920@gmail.com>
2023-08-25 15:52:19 +0530

    css fix

--- frontend/src/Pages/FileUploader.jsx
+++ frontend/src/Pages/FileUploader.jsx
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
-
+import React, { useState } from "react";
+import axios from "axios";
+import Header from "../component/Header";
+import { FcVideoFile } from "react-icons/fc";
+import { ToastContainer, toast  } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function FileUploader() {
   const [file, setFile] = useState(null);
-  const [text,setText] = useState('');
-  const [link,setLink] = useState('');
+  const [text, setText] = useState("");
+  const [link, setLink] = useState("");
+  // copy link 
+  const notify = () => toast("Copied to Clipboard!");
+
+
+  const warning = () =>{
+
+    if(file){
+      toast.success("File Uploaded!", {
+        position: toast.POSITION.TOP_RIGHT
+      });
+    }
+    else{
+      toast.warn("Upload file!", {
+        position: toast.POSITION.TOP_RIGHT
+      });
+    }
+
+  }
+
+  const copyToClipboard = () => {
+    const el = document.createElement('textarea');
+    el.value = link;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    notify();
+
+
+  };
   const handleDrop = (event) => {
     event.preventDefault();
     const droppedFile = event.dataTransfer.files[0];
@@ -23,64 +55,95 @@ function FileUploader() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-    formData.append('file', file);
-    formData.append('text',text);
+    formData.append("file", file);
+    formData.append("text", text);
     console.log(text);
-    
+
     try {
-        const response = await axios.post('https://rocket-share.onrender.com/upload', formData, {
+      const response = await axios.post(
+        "https://rockert-share-backend.onrender.com/upload",
+        formData,
+        {
           // https://fileshare-link.onrender.com/upload
           // https://rocket-share.onrender.com/upload
           headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        });
-        console.log(response.data.path);
-        console.log(response.data)
-        setLink(response.data.path);
-      } catch (err) {
-        console.error(err);
-      }
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(response.data.path);
+      console.log(response.data);
+      setLink(response.data.path);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
     <>
-    <div className='m-10'>
-      <h1 className='w-full text-center font-bold text-3xl mb-5 text-slate-600'>File Share Link!!</h1>
-    <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center'>
-      <div
-        className="border-2 border-dashed border-gray-400 p-4 rounded-md flex flex-col items-center justify-center w-full h-64"
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-      >
-        <input
-          id="file-upload"
-          name="file-upload"
-          type="file"
-          className="hidden"
-          onChange={handleFileInputChange}
-        />
-        <label htmlFor="file-upload" className="cursor-pointer">
-          {file ? file.name : "Drag and drop a file or click to choose a file"}
-        </label>
+      <div className="h-screen bg-[#EFF5FE]  ">
+        <Header />
+        <div className=" flex flex-col justify-center p-5 mt-10 bg-white rounded-md mx-60 py-10 shadow-2xl ">
+          <div className="flex flex-col  text-center mx-10   border-dashed border-cyan-950 border-2 h-96  rounded-md " >
+            <form onSubmit={handleSubmit} className="mt-10">
+              <div
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
+                className="flex flex-col items-center justify-center"
+              >
+                <FcVideoFile
+                  onClick={(e) => {
+                    document.getElementById("file-upload").click();
+                  }}
+                  size={100}
+                />
+                <input
+                  id="file-upload"
+                  name="file-upload"
+                  type="file"
+                  className="hidden"
+                  onChange={handleFileInputChange}
+                />
+
+                <label
+                  htmlFor="file-upload"
+                  className="mt-10 cursor-pointer text-blue-700 underline  text-2xl font-semibold"
+                >
+                  {file ? file.name : "Drop Your File or, Browse"}
+                </label>
+              </div>
+              <div>
+                <label className="text-xl font-bold">Password: </label>
+                <input
+                className=" mt-10 bg-white-300 rounded-md  text-black  border-2 border-black my-2  py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent  "
+                placeholder="Enter Password"
+                  type="password"
+                  onChange={(e) => {
+                    setText(e.target.value);
+                  }}
+                />
+              </div>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" type="submit" disabled={!file} >
+                Get Link
+              </button>
+            </form>
+          </div>
+
+          <div className=" flex flex-col text-center justify-center ">
+            <h1 className="text-xl font-medium ">Download Link: </h1>
+            <a className="text-blue-700 underline text-xl mt-2" href={link}>{link}</a>
+            <div className="mt-4">
+
+            {
+              link && <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 " onClick={copyToClipboard}  >copy link</button>
+
+            }
+             <ToastContainer />
+            
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-      <label>Password: </label>
-      <input type='password' className='my-10 border-2 border-black rounded-sm text-lg' onChange={(e)=>{
-                  setText(e.target.value);
-      }}/> 
-      </div>
-      <button
-        type="submit"
-        className="mt-4 px-8 py-3 text-sm font-medium text-white bg-green-500 rounded-md cursor-pointer hover:bg-green-600 focus:bg-green-600"
-        disabled={!file}
-      >
-        Uplaod
-      </button>
-    </form>
-    <h1 className='font-bold text-xl'>Download Link: </h1>
-    <a href={link} className='underline text-lg text-blue-600'>{link}</a>
-    </div>
     </>
   );
 }
---
+++ frontend/src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='bg-[#EFF5FE]  flex justify-center pt-20'>
+      <h1 className='text-4xl font-extrabold text-[#3B82F6]'>Rocket Share</h1>
+    </div>
+  )
+}
+
+export default Header
